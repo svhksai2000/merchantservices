24ff65445b24eb3c9a4decf7c38a9839505e87a6
svhksai2000 <140139526+svhksai2000@users.noreply.github.com>
2023-08-11 15:28:48 +0530

    Update Merchant.js

--- src/components/Merchant.js
+++ src/components/Merchant.js
@@ -59,3 +59,31 @@ export const Merchant = () => {
     </div>
   );
 }
+
+// return (
+//         <div class="d-inline-flex gap-1 d-md-flex justify-content-md-center">
+//             <div class="d-flex justify-content-center">
+//                 <button onClick={submitform} type="button" class="btn btn-primary std" >
+//                     Add
+//                 </button>
+//                 <form className="justify-content-center" onSubmit={(e) => {
+//                     e.preventDefault();
+//                 }}>
+//                     <div class="input-group">
+//                     <span class="input-group-text" id="basic-addon1">
+//                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+//                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+//                         </svg>
+//                     </span>
+//                     <input type="text" class="form-control" placeholder="Search Merchant by Id" aria-label="Search" aria-describedby="basic-addon1" onChange={(e) => {
+//                         e.preventDefault();
+//                         setName(e.target.value);
+
+//                         handleNameSearch(name);
+//                     }} />
+//                 </div>
+//                 </form>   
+//             </div>
+//         </div>
+
+//     )
